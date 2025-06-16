e664b56e6174d05c3a3b4cddd0bf372f08049f1b
Debs07 <deboragabrieli07@gmail.com>
2025-06-16 15:25:41 +0000

    Salvando informações da estrutura do código

---
+++ tests/test_login.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium. webdriver.common.by import By
+import pytest
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+def test_login_sucesso(driver):
+    driver.get("https://the-internet.herokuapp.com/login")
+    driver.find_element(By.ID, "username").send_keys("tomsmith")
+    driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    driver.find_element(By.CSS_SELECTOR, "button[tye='submit]").click()
+    assert "You logged into a secure area!" in driver.page_source
+
